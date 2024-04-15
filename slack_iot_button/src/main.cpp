8e7e53afa3bd3f6c1db1fff80eaeb53094ee1ef9
Viljami Vastamäki <viljami.vastamaki@tutanota.com>
2024-04-16 01:27:21 +0300

    Update GPIO pins

--- slack_iot_button/src/main.cpp
+++ slack_iot_button/src/main.cpp
@@ -4,50 +4,42 @@
 
 #include <Config.h>
 
-#define LED 8
-#define BUTTON_1_PIN 4
-#define BUTTON_2_PIN 21
-
 void setup()
 {
   Serial.begin(115200);
 
-  pinMode(BUTTON_1_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_2_PIN, INPUT_PULLUP);
+  pinMode(GPIO_NUM_4, INPUT_PULLUP);
+  pinMode(GPIO_NUM_3, INPUT_PULLUP);
 
-  pinMode(LED, OUTPUT);
+  pinMode(GPIO_NUM_8, OUTPUT);
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
-    digitalWrite(LED, HIGH);
-    delay(500);
-    digitalWrite(LED, LOW);
+    delay(200);
+    Serial.println("Connecting to WiFi..");
   }
 
-  digitalWrite(LED, LOW);
+  digitalWrite(GPIO_NUM_8, LOW);
 }
 
 void loop()
 {
-  delay(1000);
   uint64_t wakeup_status = esp_sleep_get_gpio_wakeup_status();
 
   int GPIO = log(wakeup_status) / log(2);
 
-  Serial.println(GPIO);
-
-  if (GPIO == 4)
+  if (GPIO == 3)
   {
     sendMessage("{\"text\":\":k-market:?\"}", SLACK_WEBHOOK_URL);
   }
-  else if (GPIO == 5)
+  else if (GPIO == 4)
   {
     sendMessage("{\"text\":\"Olisiko nyt sopiva aika söydä ruokaa?\"}", SLACK_WEBHOOK_URL);
   }
 
-  esp_deep_sleep_enable_gpio_wakeup(1 << BUTTON_1_PIN, ESP_GPIO_WAKEUP_GPIO_LOW);
+  esp_deep_sleep_enable_gpio_wakeup(1 << GPIO_NUM_4, ESP_GPIO_WAKEUP_GPIO_LOW);
+  esp_deep_sleep_enable_gpio_wakeup(1 << GPIO_NUM_3, ESP_GPIO_WAKEUP_GPIO_LOW);
   esp_deep_sleep_start();
 }
